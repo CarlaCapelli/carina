1653c1e99b9b163a19f17127839ce51f55d8cbc0
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2022-04-29 17:02:46 -0300

    ts

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,50 @@ let divElementos = document.getElementById("elementos");
 let cargarProductos = () => {
   for (let i: number = 0; i < lista.length; i++) {
     let divProductos = document.createElement("div");
+    divProductos.classList.add("divProductos");
     let nombreProducto = document.createElement("p");
     nombreProducto.innerHTML = lista[i];
     let precioProducto = document.createElement("p");
     precioProducto.innerHTML = "$" + precio[i];
-    let cantidad = document.createComment("input");
+    let cantidad = document.createElement("input");
     cantidad.id = "cantidad" + [i];
     cantidad.type = "number";
     cantidad.max = String(stock[i]);
     cantidad.min = "0";
+    let btn = document.createElement("button");
+    btn.id = "btn" + [i];
+    btn.innerText = "Al carrito";
     divElementos?.appendChild(divProductos);
 
     divProductos.appendChild(nombreProducto);
     divProductos.appendChild(precioProducto);
 
     divProductos.appendChild(cantidad);
+    divProductos.appendChild(btn);
+  }
+};
+let comprarProductos = () => {
+  for (let i: number = 0; i < lista.length; i++) {
+    let boton = document.getElementById("btn" + [i]);
+    boton.addEventListener("click", () => {
+      let suma: number = 0;
+      let cantidad = document.getElementById("cantidad" + [i]);
+      let cantidadSeleccionada = Number(cantidad.value);
+
+      if (cantidadSeleccionada < 1 || cantidadSeleccionada > stock[i]) {
+        alert("No tenemos ese stock");
+      } else {
+        suma = suma + precio[i] * cantidadSeleccionada;
+
+        console.log(
+          "Cantidad: " + cantidadSeleccionada + "  de " + lista[i],
+          "el precio  es $" + precio[i]
+        );
+
+        console.log("El total de  " + lista[i] + " es de $" + suma);
+      }
+    });
   }
 };
 cargarProductos();
+comprarProductos();
